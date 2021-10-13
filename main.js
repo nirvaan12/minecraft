cd3ec576e229a0b5e39451b2e8b4cf65873b31f2
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2021-10-12 20:01:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,88 @@ function new_image(get_image){
     
     });
     }
+    window.addEventListener("keydown",mykeydown);
+
+    function mykeydown(e){
+         keypress= e.keyCode;
+         if(e.shiftKey == true && keypress == "80"){
+         blockimgheight = blockimgheight+10;
+         blockimgwidth = blockimgwidth+10;
+         document.getElementById("current_width").innerHTML=blockimgwidth;
+         document.getElementById("current_height").innerHTML=blockimgheight;
+         }
+
+
+         if(e.shiftKey == true && keypress == "77"){
+            blockimgheight = blockimgheight-10;
+            blockimgwidth = blockimgwidth-10;
+            document.getElementById("current_width").innerHTML=blockimgwidth;
+            document.getElementById("current_height").innerHTML=blockimgheight;
+            }
+
+            if(keypress == "38"){
+                up();
+            
+            }
+
+
+            if(keypress == "40"){
+                down();
+            }
+
+
+            if(keypress == "37"){
+                left();
+            
+            }
+
+            if(keypress == "39"){
+                right();
+            
+            }
+
+            
+            if(keypress == "87"){
+                new_image("wall.jpg");
+            
+            }
+            if(keypress == "71"){
+                new_image("ground.png");
+            
+            }
+
+            if(keypress == "76"){
+                new_image("light_green.png");
+            
+            }
+
+            if(keypress == "84"){
+                new_image("trunk.jpg");
+            
+            }if(keypress == "82"){
+                new_image("roof.jpg");
+            
+            }
+
+            if(keypress == "89"){
+                new_image("yellow_wall.png");
+            
+            }
+
+
+            if(keypress == "68"){
+                new_image("dark_green.png");
+            
+            }
+
+            if(keypress == "85"){
+                new_image("unique.png");
+            
+            }
+
+            if(keypress == "67"){
+                new_image("cloud.jpg");
+            
+            }
+    }
+
